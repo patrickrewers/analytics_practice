92dff3595bb22200257b7ef0087f76885fb098a6
Patrick Rewers <rewersp@gmail.com>
2018-12-04 00:35:33 -0800

    Added for loop to search through articles, saving them to a list of dictionaries, each containing the title, deck, and url of the article.

--- web_scraping/academic_industry_comparison.py
+++ web_scraping/academic_industry_comparison.py
@@ -15,6 +15,16 @@ uClient.close()
 oreilly_soup = soup(oreilly_html, 'html.parser')
 jds_soup = soup(jds_html, 'html.parser')
 
-# Print off first paragraph tag of each site
-print(oreilly_soup.body.p)
-print(jds_soup.body.p)
+# Downloads text groups from websites, and removes introduction to leave only articles
+articles = oreilly_soup.findAll('div', {'class': 'text-group'})
+articles = articles[1:]
+
+# Loop through articles, and store title, deck, and url for each article in dictionary format
+#   At the end of each loop, append a copy of the dictionary to a list
+article_list = []
+for article in articles:
+    article_dictionary = {}
+    article_dictionary['title'] = article.a.get_text()
+    article_dictionary['deck'] = article.p.get_text()
+    article_dictionary['url'] = 'https://www.oreilly.com' + article.a['href']
+    article_list.append(article_dictionary)
