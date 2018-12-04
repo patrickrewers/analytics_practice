c6b0b1fa05764960ef47dc14b98b433c82e4a8e3
Patrick Rewers <rewersp@gmail.com>
2018-12-04 01:27:49 -0800

    Modularized code. Added Pandas import and used Pandas to make O'Reilly scraper return into a DataFrame for later use.

--- web_scraping/academic_industry_comparison.py
+++ web_scraping/academic_industry_comparison.py
@@ -1,30 +1,38 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+import pandas as pd
 
-oreilly_url = 'https://www.oreilly.com/topics/data-science'
-jds_url = 'http://www.jds-online.com/'
 
-# Opening up connection and downloading content
-uClient = uReq(oreilly_url)
-oreilly_html = uClient.read()
-uClient = uReq(jds_url)
-jds_html = uClient.read()
-uClient.close()
+# Open up connection and download content
+def download(url):
+    uClient = uReq(url)
+    html = uClient.read()
+    uClient.close()
+    return html
 
-# HTML parsing
-oreilly_soup = soup(oreilly_html, 'html.parser')
-jds_soup = soup(jds_html, 'html.parser')
 
-# Downloads text groups from websites, and removes introduction to leave only articles
-articles = oreilly_soup.findAll('div', {'class': 'text-group'})
-articles = articles[1:]
+def o_reilly_scraper():
+    # Scrape article title, deck, and url from O'Reilly Media's data science topic page, and export
+    html = download('https://www.oreilly.com/topics/data-science')
+    html_soul = soup(html, 'html.parser')
+    # Extract text groups from html, and remove introduction to leave only articles
+    articles = html_soul.findAll('div', {'class': 'text-group'})
+    articles = articles[1:]
+    # Loop through articles, and store title, deck, and url for each article in dictionary format
+    #   At the end of each loop, append a copy of the dictionary to a list
+    article_list = []
+    for article in articles:
+        article_dictionary = {}
+        article_dictionary['title'] = article.a.get_text()
+        article_dictionary['deck'] = article.p.get_text()
+        article_dictionary['url'] = 'https://www.oreilly.com' + article.a['href']
+        article_list.append(article_dictionary)
+    # Convert the article list to a DataFrame, in order to easily aggregate and analyze data later
+    return pd.DataFrame(article_list)
 
-# Loop through articles, and store title, deck, and url for each article in dictionary format
-#   At the end of each loop, append a copy of the dictionary to a list
-article_list = []
-for article in articles:
-    article_dictionary = {}
-    article_dictionary['title'] = article.a.get_text()
-    article_dictionary['deck'] = article.p.get_text()
-    article_dictionary['url'] = 'https://www.oreilly.com' + article.a['href']
-    article_list.append(article_dictionary)
+
+def jds_scraper():
+    # Scrape journal title,
+    html = download('http://www.jds-online.com/')
+    html_soul = soup(html, 'html.parser')
+    return
