01f02b3de4c77807f3c3a5be99bb53639eeaf91a
Patrick Rewers <rewersp@gmail.com>
2018-12-06 15:36:43 -0800

    Added scraper function that calls function to scrape the topics page for articles, and then calls a second function that scrapes the article URL for the author, publication date, and copy.

--- web_scraping/academic_industry_comparison.py
+++ web_scraping/academic_industry_comparison.py
@@ -1,6 +1,5 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
-import pandas as pd
 
 
 # Open up connection and download content
@@ -11,7 +10,7 @@ def download(url):
     return html
 
 
-def o_reilly_scraper():
+def oreilly_topic_scraper():
     # Scrape article title, deck, and url from O'Reilly Media's data science topic page, and export
     html = download('https://www.oreilly.com/topics/data-science')
     html_soul = soup(html, 'html.parser')
@@ -23,16 +22,32 @@ def o_reilly_scraper():
     article_list = []
     for article in articles:
         article_dictionary = {}
-        article_dictionary['title'] = article.a.get_text()
-        article_dictionary['deck'] = article.p.get_text()
+        article_dictionary['title'] = article.a.get_text().replace(',', '')
+        article_dictionary['deck'] = article.p.get_text().replace(',', '')
         article_dictionary['url'] = 'https://www.oreilly.com' + article.a['href']
         article_list.append(article_dictionary)
     # Convert the article list to a DataFrame, in order to easily aggregate and analyze data later
-    return pd.DataFrame(article_list)
+    return article_list
 
 
-def jds_scraper():
-    # Scrape journal title,
-    html = download('http://www.jds-online.com/')
-    html_soul = soup(html, 'html.parser')
-    return
+def oreilly_copy_scraper(article):
+    # Scrape author, publication date, and copy for each O'Reilly article provided
+    html = download(article['url'])
+    html_soup = soup(html, 'html.parser')
+    article['author'] = html_soup.findAll('span', {'class': 'author'})[0].a.get_text().replace(',', '')
+    article['date'] = html_soup.findAll('time', {'class': 'date'})[0].get_text().replace(',', '')
+    html_copy = html_soup.findAll('div', {'class': 'article-body'})[0].findAll({'p': True})[1:]
+    cleaned_copy = ''
+    for tag in html_copy:
+        cleaned_copy += tag.get_text().replace(',', '') + ('\n')
+    article['copy'] = cleaned_copy
+    return article
+
+
+
+def scraper():
+    # Scrapes data from leading Data Science publications and compares them
+    article_list = oreilly_topic_scraper()
+    for article in article_list:
+        article = oreilly_copy_scraper(article)
+    return article_list
